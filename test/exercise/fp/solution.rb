3834b0302c55497836520d35062c484ccbf89f14
m4thod0x <max.matweew@gmail.com>
2021-11-18 00:43:42 +0300

    Fix tasks with using method select

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -1,23 +1,28 @@
 module Exercise
   module Fp
     class << self
+      def check_condition(film)
+        # checking the conditions for the selection of films
+        # for the first task
+        !film['rating_kinopoisk'].nil? && !film['rating_kinopoisk'].to_f.zero? &&
+          !film['country'].nil? && (film['country'].split(',').count >= 2)
+      end
+
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
-        mapped_arr = array.map do |film|
-          rating_condition = !film['rating_kinopoisk'].nil? && !film['rating_kinopoisk'].to_f.zero?
-          country_condition = !film['country'].nil? && (film['country'].split(',').count >= 2)
-          film['rating_kinopoisk'].to_f if country_condition && rating_condition
-        end.compact
+        # film ratings selected in the chain of functions select and map
+        # then calculate average rating
+        mapped_arr = array.select { |film| check_condition(film) }
+                          .map { |film| film['rating_kinopoisk'].to_f }
         mapped_arr.reduce(0) { |sum, rating| sum + rating } / mapped_arr.count
       end
 
       def chars_count(films, threshold)
-        mapped_arr = films.map do |film|
-          film['name'] if !film['rating_kinopoisk'].nil? && (film['rating_kinopoisk'].to_f >= threshold)
-        end.compact
-        mapped_arr.reduce(0) { |sum, name| sum + name.count('и') }
+        films.select { |film| film['rating_kinopoisk'] && (film['rating_kinopoisk'].to_f >= threshold) }
+             .map { |film| film['name'] }
+             .reduce(0) { |sum, name| sum + name.count('и') }
       end
     end
   end
