cd0d1121a5b18a3883a27e629aff89bb6a1986b9
m4thod0x <max.matweew@gmail.com>
2021-11-19 13:56:10 +0300

    Solution of tasks fp2

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,16 +5,34 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each(&block)
+        return if empty?
+
+        return self if block.nil?
+
+        head, *tail = self
+        block.call(head)
+        MyArray.new(tail).my_each(&block)
+        self # return the same value for use in the chain of functions
+      end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map
+        my_reduce(MyArray.new) { |result, el| result << yield(el) }
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact
+        result = MyArray.new
+        my_each { |el| result << el unless el.nil? }
+        result
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(acc = nil)
+        my_each { |el| acc = acc.nil? ? el : yield(acc, el) }
+        acc
+      end
     end
   end
 end
